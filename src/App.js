177ae8f729962fbdcf212360b4e8a9711f3e5cd8
vijayarani <vijayarani87.s@gmail.com>
2020-07-15 00:23:21 -0500

    Added grid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Nav';
 import SideBar from "./components/navbar/sidebar";
+import HomeDashboard from './components/home/homedashboard';
 function App() {
   return (
     <div>
@@ -10,7 +11,7 @@ function App() {
       <SideBar/>
       <Navbar/>
       <main style={{marginTop:'66px'}}>
-      <p>Here is the main content</p>
+      <HomeDashboard/>
       </main>
     </div>
   );
