d09da77c2e3555b7cbfd08f2650c8130da0cfd6a
Hariharan S <bugs@gmail.com>
2020-07-18 22:55:29 -0400

    Added Logo and aligned clock

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import './App.css';
 import Navbar from './components/navbar/Nav';
 import SideBar from "./components/navbar/sidebar";
 import HomeDashboard from './components/home/homedashboard';
+import Clock from "./components/clock/clock"
+import Logo from './components/Logo/Logo';
 function App() {
   return (
     <div>
       
       <SideBar/>
+
       <Navbar/>
+      <Logo/>
       <main style={{marginTop:'66px'}}>
       <HomeDashboard/>
+  
+      <Clock/>      
       </main>
     </div>
   );
